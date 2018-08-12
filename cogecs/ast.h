84f0e61b1ed63a15fba95b22f04373d95e8f1ff3
pdelak <przemyslaw.delewski@gmail.com>
2018-08-12 23:11:16 +0200

    depth added

--- cogecs/ast.h
+++ cogecs/ast.h
@@ -8,42 +8,58 @@ struct Statement
 {
 	size_t scope = 0;
 	virtual ~Statement() {}
-	virtual void dump() const = 0;
+	virtual void dump(size_t& depth) const = 0;
 };
 
 
 struct BasicStatement : public Statement
 {
-	void dump() const {}
+	void dump(size_t& depth) const {}
 };
+
+std::string getTabs(size_t depth)
+{
+	std::string tab;
+	for (int i = 0; i < depth; ++i)
+		tab.append("\t");
+	return tab;
+}
 
 struct VarDecl : public Statement
 {
 	std::string var_name;
-	void dump() const {
+	void dump(size_t& depth) const {
+		std::cout << getTabs(depth);
 		std::cout << "Variable declaration" << std::endl;
-		std::cout << "\tname:" << var_name << std::endl;
+		std::cout << getTabs(depth+1);
+		std::cout << "name:" << var_name << std::endl;
 	}
 };
 
 struct Expression : public Statement
 {
 	std::vector<std::string> elements;
-	void dump() const {
+	void dump(size_t& depth) const {
+		std::cout << getTabs(depth);
 		std::cout << "Expression" << std::endl;
-		std::cout << "\telements size:" << elements.size() << std::endl;
+		++depth;
+		std::cout << getTabs(depth);
+		std::cout << "elements size:" << elements.size() << std::endl;
 		if (elements.size() == 1) {
-			std::cout << "\tvar : " << elements[0] << std::endl;
+			std::cout << getTabs(depth);
+			std::cout << "var : " << elements[0] << std::endl;
 		}
 		int index = 0;
 		for (const auto e: elements) {
+				std::cout << getTabs(depth);
 				if (!(index % 2))
-				std::cout << "\tvar : " << e << std::endl;
+				std::cout << "var : " << e << std::endl;
 				else 
-				std::cout << "\top  : " << e << std::endl;
+				std::cout << "op  : " << e << std::endl;
 				
 				++index;
 		}
+		--depth;
 	}
 	
 };
@@ -53,11 +69,14 @@ typedef std::vector<std::shared_ptr<Statement>> StatementList;
 struct IfStatement : public Statement
 {
 	StatementList statements;
-	void dump() const {
+	void dump(size_t& depth) const {
+		std::cout << getTabs(depth);
 		std::cout << "IfStatement" << std::endl;
+		++depth;
 		for (auto const e : statements) {
-			e->dump();
+			e->dump(depth);
 		}
+		--depth;
 	}
 
 };
@@ -67,9 +86,10 @@ typedef std::vector<std::string> StatementStack;
 
 void printAST(const StatementList& statementList)
 {
+	size_t depth = 1;
 	auto begin = statementList.begin();
 	for (; begin != statementList.end(); ++begin) {
-		(*begin)->dump();
+		(*begin)->dump(depth);
 	}
 }
 
