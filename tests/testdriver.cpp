1107fae1f94b8fc0d7fede85ef6d96729f19f986
pdelak <przemyslaw.delewski@gmail.com>
2018-08-18 22:27:49 +0200

    refactoring, common part extracted

--- tests/testdriver.cpp
+++ tests/testdriver.cpp
@@ -2,66 +2,43 @@
 #include "ast.h"
 #include "gtest/gtest.h"
 
-
-TEST(CoGeCs, test1)
+void testProgram(std::string text, StatementList result)
 {
-	std::string VarText = "var a;";
-	
-	StatementList result = {
-		makeNode<VarDecl>(0, "a")
-	};
-
 	auto parser = initialize_parser();
-	auto statements = parse(parser.get(), &VarText[0], &VarText[0] + VarText.size());
-	EXPECT_EQ(statements.size(), 1);
-	
-	std::ostringstream expected;	
+	auto statements = parse(parser.get(), &text[0], &text[0] + text.size());
+	EXPECT_EQ(statements.size(), result.size());
+
+	std::ostringstream expected;
 	dumpAST(result, expected);
 	std::ostringstream parsed;
 	dumpAST(statements, parsed);
 
 	EXPECT_EQ(parsed.str(), expected.str());
+}
+
+TEST(CoGeCs, test1)
+{
+	testProgram("var a;",
+	{
+		makeNode<VarDecl>(0, "a")
+	});
 }
 
 TEST(CoGeCs, test2)
 {
-	std::string text = "a = 1;";
-		
-	StatementList result = {
-		makeNode<Expression>(Expression(0, {"a", "=", "1"}))
-	};
-
-	auto parser = initialize_parser();
-	auto statements = parse(parser.get(), &text[0], &text[0] + text.size());
-	EXPECT_EQ(statements.size(), 1);
-
-	std::ostringstream expected;
-	dumpAST(result, expected);
-	std::ostringstream parsed;
-	dumpAST(statements, parsed);
-
-	EXPECT_EQ(parsed.str(), expected.str());
+	testProgram("a = 1;",
+	{
+		makeNode<Expression>(Expression(0,{ "a", "=", "1" }))
+	});
 }
 
 TEST(CoGeCs, test3)
 {
-	std::string text = "a = 1; a = a - 1;";
-
-	StatementList result = {
+	testProgram("a = 1; a = a - 1;", 
+	{
 		makeNode<Expression>(Expression(0,{ "a", "=", "1" })),
 		makeNode<Expression>(Expression(0,{ "a", "=", "a", "-", "1" })),
-	};
-
-	auto parser = initialize_parser();
-	auto statements = parse(parser.get(), &text[0], &text[0] + text.size());
-	EXPECT_EQ(statements.size(), 2);
-
-	std::ostringstream expected;
-	dumpAST(result, expected);
-	std::ostringstream parsed;
-	dumpAST(statements, parsed);
-
-	EXPECT_EQ(parsed.str(), expected.str());
+	});
 }
 
 
