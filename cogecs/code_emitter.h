de5a1026c78874415cf3ac808395d9a41598849c
pdelak <przemyslaw.delewski@gmail.com>
2018-12-07 16:04:49 +0100

    builtin_print codegen

--- cogecs/code_emitter.h
+++ cogecs/code_emitter.h
@@ -46,6 +46,13 @@ auto emitMachineCode(const StatementList& statements)
 	dumpCode(visitor.getStatements(), std::cout);
 	X86InstrVector i_vector;
 	i_vector.push_function_prolog();
+	i_vector.push_back({ std::byte(0x68) }); // push
+	
+	i_vector.push_back(i_vector.int_to_bytes(55));
+	i_vector.push_back({ std::byte(0xB8) });  // \  mov eax, address of function
+	i_vector.push_back(i_vector.get_address(reinterpret_cast<void*>(&builtin_print)));
+	i_vector.push_back({ std::byte(0xFF), std::byte(0xD0) }); // call eax
+	i_vector.push_back({ std::byte(0x83), std::byte(0xC4), std::byte(0x04) }); // add esp, 4 (clean stack)
 
 	i_vector.push_function_epilog();
 	JitCompiler jit(i_vector);
